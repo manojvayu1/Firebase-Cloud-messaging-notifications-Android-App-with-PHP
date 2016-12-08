6d2df5337b0d439625c44582d791cf76d8dcdcea
manojvayu <manojvayu@gmail.com>
2016-12-08 13:37:26 +0530

    FCM Notifications
    send push notifications from Firebase cloud messaging service

---
+++ app/src/main/java/com/example/vayu/notifications/app/Config.java
@@ -0,0 +1,15 @@
+package com.example.vayu.notifications.app;
+
+/**
+ * Created by Rushi on 12/1/2016.
+ */
+
+public class Config {
+
+    public static final String TOPIC_GLOBAL = "global";
+    public static final String REGISTRATION_COMPLETE = "REGISTRATIONCOMPLETE";
+    public static final String PUSH_NOTIFICATION = "PUSH";
+    public static final int NOTIFICATION_ID = 100;
+    public static final int NOTIFICATION_ID_BIG_IMAGE = 101;
+    public static final String SHARED_PREF = "AH_FIREBASE";
+}
